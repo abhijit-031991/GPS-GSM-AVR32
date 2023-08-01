b455100e502eb1ce89d53d061f74ef736fde9c12
Arcturus Inc <63253164+abhijit-031991@users.noreply.github.com>
2023-08-01 16:31:12 +0530

    0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
+#define TINY_GSM_MODEM_SIM800
+#define ARDUINOJSON_USE_LONG_LONG 1
+
 #include <Arduino.h>
-#include <SPI.h>
+#include <permaDefs.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <TinyGPS++.h>
+#include <SPIMemory.h>
+#include <TinyGsmClient.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <elapsedMillis.h>
+#include <MCP79412RTC.h>
+// #include <DFRobot_LIS2DW12.h>
+#include <avr/sleep.h>
+#include <TimeLib.h>
+#include <time.h>
 
 
 void setup() {
